0cf58c6a83e31b14e7bf462843b6b45d0b4761f0
Paulina San Martin <puli.sanmartin@gmail.com>
2024-04-15 12:39:13 -0400

    Ejercicio en clases de Elecciones presidenciales

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app= express();
+const PORT = process.env.PORT || 3002;
+import router from "./routes/routes.js";
+
+
+//Midleware
+app.use("/",router)
+
+app.listen(PORT, () =>
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+);
